17fb2febe830dfe467ecc9438ce30cc769f2f0be
mabdula2004 <muhammadabdula7874747@gmail.com>
2024-07-10 23:01:49 -0700

    updated
    flutter UI design playlist video no.13(modern login UI interface) completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -13,21 +15,70 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-        debugShowCheckedModeBanner: false,
-        home:Scaffold(
-          appBar: AppBar(
-      title: Text('Flutter Demo',),
-    ),
-          body: SingleChildScrollView(
-            child: Column(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Login App'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('assets/images/login.png',
+                  height:250.0 , width:350.0),
 
-              children: [
-
-              ],
-            ),
+              Padding(padding:
+              EdgeInsets.symmetric(horizontal: 25.0),
+                child: Column(
+                  children: [
+                    SizedBox(height:0.0,),
+                    Text(
+                      'Login here',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextFormField(
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'User name',
+                      ),
+                    ),
+                    SizedBox(height: 5.0,),
+                    TextFormField(
+                      obscureText: true,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
+                    ),
+                    SizedBox(height: 18.0,),
+                    ElevatedButton(onPressed: () {},
+                      child: Text(
+                        'Login',
+                      ),
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 50.0 ,vertical: 20.0)),
+                        textStyle: MaterialStateProperty.all(
+                          TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          drawer: (),
-    ),
+        ),
+        drawer: Drawer(),
+      ),
     );
   }
 }
